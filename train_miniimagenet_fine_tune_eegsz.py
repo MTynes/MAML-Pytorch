1f2209075b1b9b8f15787b5fc549e24c43277046
MTynes <MTynes@users.noreply.github.com>
2020-08-17 14:04:39 +0200

    Add loop for second training dataset; set query set k-shot to 15

--- train_miniimagenet_fine_tune_eegsz.py
+++ train_miniimagenet_fine_tune_eegsz.py
@@ -57,17 +57,22 @@ def main():
     # batchsz here means total episode number
     train_image_directory = args.train_dir
     test_image_directory = args.test_dir
+    fine_tune_image_directory = args.fine_tune_dir
 
     mini = MiniImagenet(train_image_directory, mode='train', n_way=args.n_way, k_shot=args.k_spt,
                         k_query=args.k_qry,
                         batchsz=50, resize=args.imgsz)
+    mini_fine_tune = MiniImagenet(fine_tune_image_directory, mode='train', n_way=args.n_way, k_shot=args.k_spt,
+                                  k_query=args.k_qry,
+                                  batchsz=50, resize=args.imgsz)
     mini_test = MiniImagenet(test_image_directory, mode='test', n_way=args.n_way, k_shot=args.k_spt,
                              k_query=args.k_qry,
                              batchsz=50, resize=args.imgsz)
-
+    highest_mean_acc = 0
     for epoch in range(args.epoch//10000):
         # fetch meta_batchsz num of episode each time
         db = DataLoader(mini, args.task_num, shuffle=True, num_workers=1, pin_memory=True)
+        db_fine_tune = DataLoader(mini_fine_tune, args.task_num, shuffle=True, num_workers=1, pin_memory=True)
 
         for step, (x_spt, y_spt, x_qry, y_qry) in enumerate(db):
 
@@ -91,19 +96,54 @@ def main():
 
                 # [b, update_step+1]
                 accs = np.array(accs_all_test).mean(axis=0).astype(np.float16)
-                print('Test acc:', accs)
+                print('Epoch ', epoch, '. Test acc:', accs)
                 print('Mean test acc: ', np.mean(accs))
+                highest_mean_acc = max(np.mean(accs), highest_mean_acc)
+    print('\nRunning fine tuning with MSU dataset....')
+    for epoch in range(args.epoch_fine_tune//10000):
+        # fetch meta_batchsz num of episode each time
+        db_fine_tune = DataLoader(mini_fine_tune, args.task_num, shuffle=True, num_workers=1, pin_memory=True)
+
+        for step, (x_spt, y_spt, x_qry, y_qry) in enumerate(db_fine_tune):
+
+            x_spt, y_spt, x_qry, y_qry = x_spt.to(device), y_spt.to(device), x_qry.to(device), y_qry.to(device)
+
+            accs = maml(x_spt, y_spt, x_qry, y_qry)
+
+            if step % 30 == 0:
+                print('step:', step, '\ttraining acc:', accs)
+
+            if step % 500 == 0:  # evaluation
+                db_test = DataLoader(mini_test, 1, shuffle=True, num_workers=1, pin_memory=True)
+                accs_all_test = []
+
+                for x_spt, y_spt, x_qry, y_qry in db_test:
+                    x_spt, y_spt, x_qry, y_qry = x_spt.squeeze(0).to(device), y_spt.squeeze(0).to(device), \
+                                                 x_qry.squeeze(0).to(device), y_qry.squeeze(0).to(device)
+
+                    accs = maml.finetunning(x_spt, y_spt, x_qry, y_qry)
+                    accs_all_test.append(accs)
+
+                # [b, update_step+1]
+                accs = np.array(accs_all_test).mean(axis=0).astype(np.float16)
+                print('Epoch ', epoch, '. Test acc:', accs)
+                print('Mean test acc: ', np.mean(accs))
+                highest_mean_acc = max(np.mean(accs), highest_mean_acc)
+    print('\nHighest test accuracy: ', highest_mean_acc)
+
 
 if __name__ == '__main__':
 
     argparser = argparse.ArgumentParser()
 
     argparser.add_argument('--train_dir', type=str, help='train data directory', default='/content/miniimagenet/images')
+    argparser.add_argument('--fine_tune_dir', type=str, help='fine tuning data directory', default='/content/all_fine_tuning_images')
     argparser.add_argument('--test_dir', type=str, help='test data directory', default='/content/all_test_images')
     argparser.add_argument('--epoch', type=int, help='epoch number', default=(200 * 10000))##6
-    argparser.add_argument('--n_way', type=int, help='n way', default=2) #cannot be larger than the number of categories
+    argparser.add_argument('--epoch_fine_tune', type=int, help='epoch number', default=(200 * 10000))##6
+    argparser.add_argument('--n_way', type=int, help='n way', default=2) # cannot be larger than the number of categories
     argparser.add_argument('--k_spt', type=int, help='k shot for support set', default=1)
-    argparser.add_argument('--k_qry', type=int, help='k shot for query set', default=15)
+    argparser.add_argument('--k_qry', type=int, help='k shot for query set', default=5)#15
     argparser.add_argument('--imgsz', type=int, help='imgsz', default=84) #
     argparser.add_argument('--imgc', type=int, help='imgc', default=3)
     argparser.add_argument('--task_num', type=int, help='meta batch size, namely task num', default=4)
